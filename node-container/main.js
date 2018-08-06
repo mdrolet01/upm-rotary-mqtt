e4c6a5f821938b58727c3bd21e6102ab851a2432
Michael Drolet <mdrolet01@gmail.com>
2018-08-06 12:56:06 -0700

    update main.js

--- node-container/main.js
+++ node-container/main.js
@@ -8,24 +8,6 @@ var mqtt    = require('mqtt');
 const PORT = 8000;
 const HOST = '0.0.0.0'
 
-// Setup MongoDB
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://" + process.env.MONGODB_URL +":"+ process.env.MONGODB_PORT +"/";
-var dbo;
-
-// Connect to MongoDB - Not currently implemented
-/* MongoClient.connect(url, function(err, client) {
-   if (err) {
-   console.log("Unable to connect with MongoDB")
-   throw  err
-   }
-   else{
-   console.log("Connected successfully to MongoDB server");
-   dbo = client.db("mqtt");
-   }
-   });
- */
-
 // Connect to MQTT Broker
 var client  = mqtt.connect("mqtt://" + process.env.MQTTServer_URL, {port: 1883});
 console.log("connected flag  " + client.connected);
@@ -42,16 +24,6 @@ app.get('/', function(req, res){
 client.on('message',function(topic, message, packet){
     io.emit('updateAngle', JSON.parse(message));
     });
-
-/* ---------- ALTERNATIVE OPTION USING MONGODB ----------
-   io.on('connection', function(socket){
-   setInterval(function(){
-   dbo.collection('sensor_data').findOne({},{sort:[['_id',-1]]},function (findErr, result){
-   io.emit('chat message', result.value);
-   });
-   }, 100) 
-   });
- */ 
 
 // Setup Server on port 8000
 http.listen(PORT, HOST);
--- upm-container/app.py
+++ upm-container/app.py
@@ -7,15 +7,11 @@ import paho.mqtt.client as paho
 from time import sleep
 from upm import pyupm_grove as grove
 
-broker= os.environ['MQTTServer_URL'] # Try using mosquitto container DNS --  Use different IP Address if publishing to another device
+#Connect to MQTT Broker
+broker= os.environ['MQTTServer_URL']
 print(broker)
 port=1883
-def on_publish(client,userdata,result):
-    #print("data published \n")
-    pass
-
-client1= paho.Client("control1")
-client1.on_publish = on_publish
+client1= paho.Client("pythonClient")
 client1.connect(broker,port)
 
 def main():
@@ -28,11 +24,9 @@ def main():
         # Read values
         abs = knob.abs_value()
         absdeg = knob.abs_deg()
-        absrad = knob.abs_rad()
 
-        #print("Abs values: %4d" % int(abs) , " raw %4d" % int(absdeg), "deg = %5.2f" % absrad , " rad ", end=' ')
-
-        payload = "{\"sensor_id\":\"Temperature Sensor\",\"value\":" + str(int(absdeg)) + "}"
+        # Publish sensor data to MQTT Broker
+        payload = "{\"sensor_id\":\"Rotary Sensor\",\"value\":" + str(int(absdeg)) + "}"
         ret= client1.publish("sensors/rotary/data", payload) 
         sleep(.01)
 
